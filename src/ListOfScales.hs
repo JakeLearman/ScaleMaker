3d0c5107ff7aa1974d0c5201fd6148bfac2ddb9c
Jake Learman <jaken_learman@hotmail.com>
2019-01-05 13:13:13 +0000

    Created List File

---
+++ src/ListOfScales.hs
@@ -0,0 +1,13 @@
+module ListOfScales where
+
+whole :: Int
+whole = 2
+
+half :: Int
+half = 1
+
+major :: [Int]
+major = [whole, whole, half, whole, whole, whole, half]
+
+minor :: [Int]
+minor = [whole, half, whole, whole, half, whole, whole]
--- src/Scale.hs
+++ src/Scale.hs
@@ -1,28 +1,21 @@
 module Scale where
  
 import Data.List
+import ListOfScales
 
 notes = cycle ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"] 
 
-whole :: Int
-whole = 2
-
-half :: Int
-half = 1
-
-major :: [Int]
-major = [whole, whole, half, whole, whole, whole, half]
-
-minor :: [Int]
-minor = [whole, half, whole, whole, half, whole, whole]
-getNote :: [String] -> Int -> String
-getNote notes n = notes !! n
+-- Used to flatten a list of steps into a cumulative list of steps
 
 flattenKey :: [Int] -> [Int]
 flattenKey xs = scanl1 (+) xs
 
+-- Used to fetch the specified notes in the scale
+
 makeScale :: [String] -> [Int] -> [String]
 makeScale xs ys = [xs !! y | y <- (flattenKey ys)]
 
+-- Used to move the starting note back to the front of the sclae
+
 formatScale :: [String] -> [Int] -> [String]
-formatScale xs ys =  [last (makeScale xs ys)] ++ init (makeScale xs ys)
+formatScale xs ys =  [last (makeScale xs ys)] ++ (makeScale xs ys)
