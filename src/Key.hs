08204f1131730e1b42764819468addc2d90a39d5
Jake Learman <jaken_learman@hotmail.com>
2019-01-12 18:12:13 +0000

    Commented

--- src/Key.hs
+++ src/Key.hs
@@ -4,16 +4,24 @@ import Data.List
 import Scale
 import ListOfScales
 
+-- Used to print all scales of a certain type e.g. major, minor... full list in ListOfScales.hs
+-- Usage :: printAllOfType major = ..List of all major scales...
 printAllOfType :: [Int] -> [[String]]
 printAllOfType scaleType = do
         [scale' x scaleType | x <- notes]
 
+-- Same as above but removes duplicate scales
 tidyPrintAllOfType :: [Int] -> [[String]]
 tidyPrintAllOfType scaleType = nub (printAllOfType scaleType)
 
+-- Used to print all scales in a certain key
+-- Usage: printAllOfKey "C" would return all scales in the list in the key of C
 printAllOfKey :: String -> [[String]]
 printAllOfKey note = do
         [scale' note x | x <- listOfScales]
 
+-- Prints all scales in the list of scales in every key
+
 printAllScales :: [[[String]]]
 printAllScales = [printAllOfKey x | x <- notes]
+
